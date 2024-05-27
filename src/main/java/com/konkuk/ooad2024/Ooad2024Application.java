1ec50cec7d8109ac9f20324a88e31536e6ec8c66
jiwoo <zbqmgldjfh@gmail.com>
2024-05-27 13:08:04 +0900

    Initial commit

---
+++ src/main/java/com/konkuk/ooad2024/Ooad2024Application.java
@@ -0,0 +1,13 @@
+package com.konkuk.ooad2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ooad2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ooad2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/konkuk/ooad2024/Ooad2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konkuk.ooad2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ooad2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
