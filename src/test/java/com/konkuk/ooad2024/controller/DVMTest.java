d8955caa401dac779d4760bcda69686afdbb05d2
DS0708 <tlsehdtjd070@naver.com>
2024-06-05 18:44:04 +0900

    DVM 선결제 기능 단위 테스트 구현 완료

--- src/test/java/com/konkuk/ooad2024/controller/DVMTest.java
+++ src/test/java/com/konkuk/ooad2024/controller/DVMTest.java
@@ -1,6 +1,7 @@
 package com.konkuk.ooad2024.controller;
 
 import com.konkuk.ooad2024.domain.*;
+import com.konkuk.ooad2024.dto.PrePaymentResponseDto;
 import com.konkuk.ooad2024.service.Beverages;
 import com.konkuk.ooad2024.service.OtherDVMs;
 import com.konkuk.ooad2024.service.PaymentMachine;
@@ -35,10 +36,6 @@ public class DVMTest {
   @MockBean
   private Bank bank;
 
-  @BeforeEach
-  void setUp() {
-  }
-
   @Test
   void 컨트롤러_인증코드_음료수_반환_테스트() throws Exception{
     // Given
@@ -57,4 +54,61 @@ public class DVMTest {
             .andExpect(jsonPath("$.beverageId").value("01"))
             .andExpect(jsonPath("$.quantity").value(1));
   }
+
+  @Test
+  void 컨트롤러_선결제_성공_테스트() throws Exception {
+    // Given
+    long accountId = 1L;
+    BeverageName beverageName = BeverageName.COKE;
+    int quantity = 2;
+    long price = 500;
+    long totalAmount = quantity * price;
+    Position position = new Position(10, 20);
+    PrePaymentResponseDto responseDto = new PrePaymentResponseDto(true, "abcdefghij");
+
+    String jsonRequest = String.format("{\"accountId\": %d, \"beverageId\": \"%s\", \"quantity\": %d, \"x\": %d, \"y\": %d}",
+            accountId, beverageName.getCode(), quantity, position.getXaxis(), position.getYaxis());
+
+    // When
+    when(beverages.findPriceByName(beverageName)).thenReturn(price);
+    when(bank.balanceCheck(accountId, totalAmount)).thenReturn(true);
+    when(otherDVMs.findByPosition(any(Position.class))).thenReturn(new OtherDVM(position,"127.0.0.1",9999,"1"));
+    when(paymentMachine.prePayment(any(Position.class), any(Beverage.class))).thenReturn(responseDto);
+
+    // Then
+    mockMvc.perform(post("/prepay")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.haveBalance").value(true))
+            .andExpect(jsonPath("$.authenticationCode").value("abcdefghij"));
+  }
+
+  @Test
+  void 컨트롤러_선결제_잔액부족_테스트() throws Exception {
+    // Given
+    long accountId = 1L;
+    BeverageName beverageName = BeverageName.COKE;
+    int quantity = 2;
+    long price = 500;
+    long totalAmount = quantity * price;
+    Position position = new Position(10, 20);
+
+    String jsonRequest = String.format("{\"accountId\": %d, \"beverageId\": \"%s\", \"quantity\": %d, \"x\": %d, \"y\": %d}",
+            accountId, beverageName.getCode(), quantity, position.getXaxis(), position.getYaxis());
+
+    // When
+    when(beverages.findPriceByName(beverageName)).thenReturn(price);
+    when(bank.balanceCheck(accountId, totalAmount)).thenReturn(false);
+
+    // Then
+    mockMvc.perform(post("/prepay")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.haveBalance").value(false))
+            .andExpect(jsonPath("$.authenticationCode").doesNotExist());
+  }
+
+
 }
